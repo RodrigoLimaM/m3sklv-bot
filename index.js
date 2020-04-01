80e9a4d6f798e0c2e335878ecc99a72cd53570ac
Rodrigo Lima <digo100200@gmail.com>
2020-04-01 00:57:53 -0300

    NEW greetings

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ require('dotenv').config()
 const { Client } = require('discord.js')
 const client = new Client()
 const sort = require('./commands/sort')
+const greetings = require('./commands/greetings')
 
 client.on('ready', () => {
   console.log(`Logado como ${client.user.tag}`)
 })
 
-
 client.on('message', msg => {
   sort(msg)
+})
+
+client.on('message', msg => {
+  greetings(msg)
 })
 
 client.login(process.env.TOKEN_DISCORD)
