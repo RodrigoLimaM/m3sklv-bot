71fe3cc18387ed744763038b00a5a268b91709ef
Rodrigo Lima <digo100200@gmail.com>
2020-04-05 20:35:21 -0300

    correcoes search

--- commands/commands.js
+++ commands/commands.js
@@ -11,7 +11,7 @@ module.exports = function commands(msg) {
                         **!ajuda, !help, !comandos ou !commands** - Mostra todos os comandos do nosso bot;
                         **!sorteio** - Realiza sorteio para X5;
                         **!clima, !tempo ou !weather** - Mostra a previsão do tempo de hoje para São Paulo;
-                        **!search ou !pesquisa** - Realiza e mostra os resultados de uma pesquisa.
+                        **!search, !pesquisar ou !pesquisa** - Realiza e mostra os resultados de uma pesquisa.
                         `)
                         .setFooter('Criado com ❤ por M3SKLV T3CH')
                         .setColor('#fff200'))
